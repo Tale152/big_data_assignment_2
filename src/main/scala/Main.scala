d33b6bd6dd68b66cea4c9f3449a73d475e822520
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-02-21 11:05:10 +0000

    Added first implementation of K-means algorithm

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,5 @@
 import org.apache.spark.{SparkConf, SparkContext}
+import eCP.Java.SiftDescriptorContainer
 
 object Main {
     
@@ -6,13 +7,33 @@ object Main {
     val masterAddress = "spark://192.168.1.82:7077"
     val jarPath = "target\\scala-2.12\\app_2.12-1.0.jar"
 
+    val centroidNumber = 10
+
     def main(args: Array[String]){
 
         println("STARTING COMPUTATION")
         val sc = ContextFactory.create(appName, masterAddress, jarPath)
 
         val rdd = DataLoader.loadSIFTs(sc, "C:\\Users\\teemo\\Desktop\\big_data_assignment_2\\bigann_query.seq")
-        println(rdd.count)
+        
+        val randomCentroids = rdd.take(centroidNumber)
+
+        val broadcastCentroids = sc.broadcast(randomCentroids)
+        broadcastCentroids.value.map(point => println(point))
+
+        val distRDD = rdd.map(point => {
+            import EuclideanDistance.distance
+
+	        val pointsDistance = broadcastCentroids.value.map(centroid => (centroid.id, distance(centroid, point)))
+	        val sortedPointsDistance = pointsDistance.sortBy(_._2)
+	        (sortedPointsDistance(0)._1, 1)
+        })
+
+        val results = distRDD.reduceByKey( _ + _ ).collect
+
+        results.sortBy(_._1).map(println)
+
+        broadcastCentroids.unpersist
 
         sc.stop()
         println("COMPUTATION ENDED")
