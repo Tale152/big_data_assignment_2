10e2b563563f3868aed27454e26d00a08c2617f4
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-02-21 20:14:30 +0000

    Completed kmeans organization

---
+++ src/main/scala/KMeansBuilder.scala
@@ -0,0 +1,44 @@
+import eCP.Java.SiftDescriptorContainer
+import org.apache.spark.SparkContext
+import org.apache.spark.broadcast.Broadcast
+import org.apache.spark.rdd.RDD
+
+case class KMeansBuilder() {
+
+  private var path = Option.empty[String]
+  private var sc = Option.empty[SparkContext]
+  private var initSelector = Option.empty[RDD[SiftDescriptorContainer] => Array[SiftDescriptorContainer]]
+  private var mr = Option.empty[(RDD[SiftDescriptorContainer], Broadcast[Array[SiftDescriptorContainer]]) => Array[SiftDescriptorContainer]]
+  private var ec = Option.empty[Int => Boolean]
+
+  def dataPath(path: String): KMeansBuilder ={
+    this.path = Option(path)
+    this
+  }
+
+  def sparkContext(sc: SparkContext): KMeansBuilder ={
+    this.sc = Option(sc)
+    this
+  }
+
+  def initCentroidSelector(initSelector: RDD[SiftDescriptorContainer] => Array[SiftDescriptorContainer]): KMeansBuilder ={
+    this.initSelector = Option(initSelector)
+    this
+  }
+
+  def mapReduce(mr: (RDD[SiftDescriptorContainer], Broadcast[Array[SiftDescriptorContainer]]) => Array[SiftDescriptorContainer]): KMeansBuilder ={
+    this.mr = Option(mr)
+    this
+  }
+
+  def endCondition(ec: Int => Boolean): KMeansBuilder ={
+    this.ec = Option(ec)
+    this
+  }
+
+  def build(): KMeansComputation = {
+    //TODO check no Option.empty
+    KMeansComputation(sc.get, path.get, initSelector.get, mr.get, ec.get)
+  }
+
+}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,7 @@
-import org.apache.spark.{SparkConf, SparkContext}
+import EuclideanDistance.distance
 import eCP.Java.SiftDescriptorContainer
+import org.apache.spark.broadcast.Broadcast
+import org.apache.spark.rdd.RDD
 
 object Main {
     
@@ -8,34 +10,70 @@ object Main {
     val jarPath = "target\\scala-2.12\\app_2.12-1.0.jar"
 
     val centroidNumber = 10
+    val iterationNumber = 20
 
     def main(args: Array[String]){
 
         println("STARTING COMPUTATION")
         val sc = ContextFactory.create(appName, masterAddress, jarPath)
 
-        val rdd = DataLoader.loadSIFTs(sc, "C:\\Users\\teemo\\Desktop\\big_data_assignment_2\\bigann_query.seq")
-        
-        val randomCentroids = rdd.take(centroidNumber)
+       val kMeans = KMeansBuilder()
+          .sparkContext(sc)
+          .dataPath("C:\\Users\\teemo\\Desktop\\big_data_assignment_2\\bigann_query.seq")
+          .initCentroidSelector(rdd => rdd.take(centroidNumber))
+          .mapReduce(mr)
+          .endCondition(iteration => iteration == iterationNumber)
+          .build()
 
-        val broadcastCentroids = sc.broadcast(randomCentroids)
-        broadcastCentroids.value.map(point => println(point))
+        val result = kMeans.compute
 
-        val distRDD = rdd.map(point => {
-            import EuclideanDistance.distance
-
-	        val pointsDistance = broadcastCentroids.value.map(centroid => (centroid.id, distance(centroid, point)))
-	        val sortedPointsDistance = pointsDistance.sortBy(_._2)
-	        (sortedPointsDistance(0)._1, 1)
-        })
-
-        val results = distRDD.reduceByKey( _ + _ ).collect
-
-        results.sortBy(_._1).map(println)
-
-        broadcastCentroids.unpersist
+        println(result.length)
 
         sc.stop()
         println("COMPUTATION ENDED")
     }
+
+  def sumArray(x: Array[Byte], y: Array[Byte]): Array[Int] ={
+    var i = 0
+    x.map(a => {
+
+      val res = a + y(i)
+      i += 1
+      res
+    })
+  }
+
+  private case class IdGenerator(){
+    private var i = 0
+    def getNextId: Int = {
+      val res = i
+      i += 1
+      res
+    }
+  }
+
+  def divideVector(vector: Array[Double], n: Int): Array[Byte] = vector.map(x => (x/n).asInstanceOf[Byte])
+
+  def sumVectors(x: Array[Double], y: Array[Double]): Array[Double] = {
+    var i = 0
+    x.map(e => {
+      val r = e + y(i)
+      i += 1
+      r
+    })
+  }
+
+  def mr: (RDD[SiftDescriptorContainer], Broadcast[Array[SiftDescriptorContainer]]) => Array[SiftDescriptorContainer] = {
+    val idGenerator = IdGenerator()
+    (rdd, broadcastCentroids) => rdd
+      .map(point => {
+        val pointsDistance = broadcastCentroids.value.map(centroid => (centroid.id, distance(centroid, point)))
+        val sortedPointsDistance = pointsDistance.sortBy(_._2)
+        (sortedPointsDistance(0)._1, (1, point.vector.map(x => x.asInstanceOf[Double])))
+      })
+      .reduceByKey((x,y) => (x._1 + y._1, sumVectors(x._2, y._2)))
+      .map(v => new SiftDescriptorContainer(idGenerator.getNextId, divideVector(v._2._2, v._2._1)))
+      .collect()
+  }
+
 }
