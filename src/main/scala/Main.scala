cc82e7165170faa6e218570bdbaf7e8f4613c4a2
Tale152 <alessandro.talmi@studio.unibo.it>
2022-02-23 18:36:03 +0000

    KISS: created Computation object to slim Main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,49 +1,16 @@
-import kMeans.KMeans
-import kMeans.versions.EndConditions.{endByMaxReached, endBySimilarity}
-import kMeans.versions.InitCentroidSelectors.useFirstN
-import kMeans.versions.MapReduces.{baseMapReduce, earlyHaltingMapReduce}
-import org.apache.spark.SparkContext
+import kMeans.Computation.{kMeans, printConfiguration}
 import utils.{Const, ContextFactory}
 import utils.LogEnabler.logSelectedOption
-import utils.ArgsProvider.{centroidSelector, centroidsNumber, dataPath, endCondition, logFlag, mapReduce, setArgs, sparkMaster}
+import utils.ArgsProvider
 
 object Main {
 
     def main(args: Array[String]): Unit = {
-      setArgs(args)
-      logSelectedOption(logFlag)
-      val sc = ContextFactory.create(Const.appName, sparkMaster, Const.jarPath)
+      ArgsProvider.setArgs(args)
+      logSelectedOption(ArgsProvider.logFlag)
+      printConfiguration()
+      val sc = ContextFactory.create(Const.appName, ArgsProvider.sparkMaster, Const.jarPath)
       kMeans(sc)
       sc.stop()
     }
-
-  private def kMeans(sc: SparkContext): Unit = {
-    printConfiguration()
-    val kMeansExecutor = KMeans(sc, centroidsNumber, dataPath)
-    centroidSelector match {
-      case "FIRST_N" => kMeansExecutor.setInitCentroidSelector(useFirstN)
-      case _ => throw new IllegalArgumentException("The specified centroid selector does not exist")
-    }
-    mapReduce match {
-      case "DEFAULT" => kMeansExecutor.setMapReduce(baseMapReduce)
-      case "EARLY_HALTING" => kMeansExecutor.setMapReduce(earlyHaltingMapReduce)
-      case _ => throw new IllegalArgumentException("The specified map-reduce does not exist")
-    }
-    endCondition match {
-      case "MAX" => kMeansExecutor.setEndCondition(endByMaxReached)
-      case "SIMILARITY" => kMeansExecutor.setEndCondition(endBySimilarity)
-      case _ => throw new IllegalArgumentException("The specified end condition does not exist")
-    }
-    kMeansExecutor.compute()
-  }
-
-  private def printConfiguration(): Unit = {
-    println("==========================")
-    println("Running K-Means with:\n")
-    println("Centroid selector: " + centroidSelector)
-    println("Centroid number: " + centroidsNumber)
-    println("Map-Reduce algorithm: " + mapReduce)
-    println("End condition: " + endCondition)
-    println("==========================\n")
-  }
 }
---
+++ src/main/scala/kMeans/Computation.scala
@@ -0,0 +1,39 @@
+package kMeans
+
+import kMeans.versions.EndConditions.{endByMaxReached, endBySimilarity}
+import kMeans.versions.InitCentroidSelectors.useFirstN
+import kMeans.versions.MapReduces.{baseMapReduce, earlyHaltingMapReduce}
+import org.apache.spark.SparkContext
+import utils.ArgsProvider
+
+object Computation {
+
+  def kMeans(sc: SparkContext): Unit = {
+    val kMeansExecutor = KMeans(sc, ArgsProvider.centroidsNumber, ArgsProvider.dataPath)
+    ArgsProvider.centroidSelector match {
+      case "FIRST_N" => kMeansExecutor.setInitCentroidSelector(useFirstN)
+      case _ => throw new IllegalArgumentException("The specified centroid selector does not exist")
+    }
+    ArgsProvider.mapReduce match {
+      case "DEFAULT" => kMeansExecutor.setMapReduce(baseMapReduce)
+      case "EARLY_HALTING" => kMeansExecutor.setMapReduce(earlyHaltingMapReduce)
+      case _ => throw new IllegalArgumentException("The specified map-reduce does not exist")
+    }
+    ArgsProvider.endCondition match {
+      case "MAX" => kMeansExecutor.setEndCondition(endByMaxReached)
+      case "SIMILARITY" => kMeansExecutor.setEndCondition(endBySimilarity)
+      case _ => throw new IllegalArgumentException("The specified end condition does not exist")
+    }
+    kMeansExecutor.compute()
+  }
+
+  def printConfiguration(): Unit = {
+    println("==========================")
+    println("Running K-Means with:\n")
+    println("Centroid selector: " + ArgsProvider.centroidSelector)
+    println("Centroid number: " + ArgsProvider.centroidsNumber)
+    println("Map-Reduce algorithm: " + ArgsProvider.mapReduce)
+    println("End condition: " + ArgsProvider.endCondition)
+    println("==========================\n")
+  }
+}
