bc144b144a39a4bb9bc1da170303f960e917b876
Tale152 <alessandro.talmi@studio.unibo.it>
2022-02-24 00:28:32 +0000

    refactored sum and similarity

--- src/main/scala/kMeans/VectorOperation.scala
+++ src/main/scala/kMeans/VectorOperation.scala
@@ -7,23 +7,9 @@ object VectorOperation {
 
   def divide(vector: Array[Double], n: Int): Array[Byte] = vector.map(x => (x / n).asInstanceOf[Byte])
 
-  def sum(x: Array[Double], y: Array[Double]): Array[Double] = {
-    var i = 0
-    val res = x.map(e => {
-      val r = e + y(i)
-      i += 1
-      r
-    })
-    res
-  }
+  def sum(x: Array[Double], y: Array[Double]): Array[Double] = (x,y).zipped.map((a,b) => a + b)
 
-  def similarity(x: Array[SiftDescriptorContainer], y: Array[SiftDescriptorContainer], tolerance: Int): Boolean = {
-    var i = 0
-    x.forall(e => {
-      val res = Math.abs(distance(e, y(i))) < tolerance
-      i += 1
-      res
-    })
-  }
+  def similarity(x: Array[SiftDescriptorContainer], y: Array[SiftDescriptorContainer], tolerance: Int): Boolean =
+    (x,y).zipped.forall((a,b) => Math.abs(distance(a,b)) < tolerance)
 
 }
