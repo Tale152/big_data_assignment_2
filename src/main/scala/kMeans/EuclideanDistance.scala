8420e8ae470ab5cda4e41d3ae849089d8d4a392a
Tale152 <alessandro.talmi@studio.unibo.it>
2022-02-24 23:47:34 +0000

    implemented early halting unraveling map-reduce

--- src/main/scala/kMeans/EuclideanDistance.scala
+++ src/main/scala/kMeans/EuclideanDistance.scala
@@ -16,16 +16,47 @@ object EuclideanDistance {
     ret
   }
 
-  def distance(a: Point, b: Point, currMin: Int): Int = {
+  def distance(a: Point, b: Point, currMin: Int, lateChecksIndex: Int): Int = {
     var ret = 0
     var i = 0
-    while (i < a.vector.length) {
+    while (i < lateChecksIndex) {
       val t = a.vector(i) - b.vector(i)
       ret += t * t
+      i = i + 1
+    }
+    while (i < a.vector.length) {
       if(ret > currMin){
         return Integer.MAX_VALUE
       }
+      val t = a.vector(i) - b.vector(i)
+      ret += t * t
       i = i + 1
+    }
+    ret
+  }
+
+  def distance(a: Point, b: Point, currMin: Int, lateChecksIndex: Int, isVectorLenOdd: Boolean): Int = {
+    var ret = 0
+    var i = 0
+    while(i < lateChecksIndex){
+      val t1 = a.vector(i) - b.vector(i)
+      val t2 = a.vector(i + 1) - b.vector(i + 1)
+      ret += (t1 * t1) + (t2 * t2)
+      i = i + 2
+    }
+    if(isVectorLenOdd){
+      val t = a.vector(i) - b.vector(i)
+      ret += (t * t)
+      i = i + 1
+    }
+    while (i < a.vector.length) {
+      if(ret > currMin){
+        return Integer.MAX_VALUE
+      }
+      val t1 = a.vector(i) - b.vector(i)
+      val t2 = a.vector(i + 1) - b.vector(i + 1)
+      ret += (t1 * t1) + (t2 * t2)
+      i = i + 2
     }
     ret
   }
--- src/main/scala/kMeans/versions/MapReduces.scala
+++ src/main/scala/kMeans/versions/MapReduces.scala
@@ -20,12 +20,13 @@ object MapReduces {
       .collect()
 
 
-  def earlyHaltingMapReduce(data: RDD[Point], centroids: Broadcast[Array[Point]]): Array[Point] =
+  def earlyHaltingMapReduce(data: RDD[Point], centroids: Broadcast[Array[Point]]): Array[Point] = {
+    val lateChecksIndex: Int = data.take(1)(0).vector.length / 4
     data
       .map(point => {
         var minDist = Integer.MAX_VALUE
         val pointsDistance = centroids.value.map(centroid => {
-          val pointCentroidDistance = distance(centroid, point, minDist)
+          val pointCentroidDistance = distance(centroid, point, minDist, lateChecksIndex)
           if(pointCentroidDistance < minDist){
             minDist = pointCentroidDistance
           }
@@ -37,5 +38,31 @@ object MapReduces {
       .reduceByKey((x, y) => (x._1 + y._1, sum(x._2, y._2)))
       .map(v => Point(v._1, divide(v._2._2, v._2._1)))
       .collect()
+  }
+
+  def earlyHaltingUnravelingMapReduce(data: RDD[Point], centroids: Broadcast[Array[Point]]): Array[Point] = {
+    val vectorLen = data.take(1)(0).vector.length
+    var lateChecksIndex: Int = vectorLen / 4
+    if(lateChecksIndex % 2 != 0){
+      lateChecksIndex += 1 //ensuring that it is even
+    }
+    val isVectorLenOdd = vectorLen % 2 != 0
+    data
+      .map(point => {
+        var minDist = Integer.MAX_VALUE
+        val pointsDistance = centroids.value.map(centroid => {
+          val pointCentroidDistance = distance(centroid, point, minDist, lateChecksIndex, isVectorLenOdd)
+          if(pointCentroidDistance < minDist){
+            minDist = pointCentroidDistance
+          }
+          (centroid.id, pointCentroidDistance)
+        })
+        val sortedPointsDistance = pointsDistance.sortBy(_._2)
+        (sortedPointsDistance(0)._1, (1, point.vector.map(x => x.asInstanceOf[Double])))
+      })
+      .reduceByKey((x, y) => (x._1 + y._1, sum(x._2, y._2)))
+      .map(v => Point(v._1, divide(v._2._2, v._2._1)))
+      .collect()
+  }
 
 }
