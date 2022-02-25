065b825945c3af71e8547c19e3d26db03a752c81
Tale152 <alessandro.talmi@studio.unibo.it>
2022-02-25 10:32:31 +0000

    documented LogEnabler

--- src/main/scala/utils/LogEnabler.scala
+++ src/main/scala/utils/LogEnabler.scala
@@ -2,8 +2,15 @@ package utils
 
 import org.apache.log4j.{Level, Logger}
 
+/**
+ * Utility object that contains a method to turn on/off the Spark's Logger.
+ */
 object LogEnabler {
 
+  /**
+   * Turns on/off the Spark's Logger.
+   * @param flag that turns on/off the Spark's Logger
+   */
   def logSelectedOption(flag: Boolean): Unit = if(!flag){
       Logger.getLogger("org").setLevel(Level.OFF)
       Logger.getLogger("akka").setLevel(Level.OFF)
