90c271b2fcbcfb876132e61fce1a392de34a805c
Tale152 <alessandro.talmi@studio.unibo.it>
2022-02-25 10:34:33 +0000

    Documented Point

--- src/main/scala/utils/Point.scala
+++ src/main/scala/utils/Point.scala
@@ -1,3 +1,8 @@
 package utils
 
+/**
+ * Class that wraps data extracted by .seq file.
+ * @param id Int id of the point
+ * @param vector Array of Bytes with the coordinates of the Point
+ */
 case class Point(id: Int, vector: Array[Byte]) extends Serializable
