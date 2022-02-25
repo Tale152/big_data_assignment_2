3153d8e411976e032882af1090b66084887bc52b
Tale152 <alessandro.talmi@studio.unibo.it>
2022-02-25 10:58:55 +0000

    corrected ContextFactory documentation

--- src/main/scala/utils/ContextFactory.scala
+++ src/main/scala/utils/ContextFactory.scala
@@ -3,7 +3,7 @@ package utils
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
- * Utility object that provides a method to create a SparkContext
+ * Utility object that provides a method to create a [[SparkContext]]
  */
 object ContextFactory {
 
@@ -11,7 +11,7 @@ object ContextFactory {
      * @param appName name that will be used for the computation in the Spark cluster
      * @param masterAddress address of the master that coordinates the Spark cluster
      * @param jarPath path where the Jar of this program (used by the Spark cluster's Workers) is stored
-     * @return the newly created SparkContext
+     * @return the newly created [[SparkContext]]
      */
     def create(appName: String, masterAddress: String, jarPath: String): SparkContext = new SparkContext(
         new SparkConf().setAppName(appName).setMaster(masterAddress).setJars(Seq(jarPath))
