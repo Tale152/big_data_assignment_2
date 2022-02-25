368ed00a8067f687c0c49c0b4657acd2e5a49275
Tale152 <alessandro.talmi@studio.unibo.it>
2022-02-25 10:27:50 +0000

    documented DataLoader

--- src/main/scala/utils/DataLoader.scala
+++ src/main/scala/utils/DataLoader.scala
@@ -4,17 +4,19 @@ import org.apache.hadoop.io.{BytesWritable, IntWritable}
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 
+/**
+ * Utility object that contains methods used to load the data that will be analyzed by the program.
+ */
 object DataLoader {
 
   /**
    * This function is specificly designed to load a file of SIFT descriptors into an RDD.
    *
-   * @param sc       SparkContext of the master node.
+   * @param sc SparkContext of the master node.
    * @param fileName path to the file of SIFT descriptors.
    * @return RDD of all the SIFT's in the file (using the key as the SIFT's ID).
    */
-  def loadSIFTs(sc: SparkContext, fileName: String): RDD[Point] =
-    sc
-      .sequenceFile(fileName, classOf[IntWritable], classOf[BytesWritable])
-      .map(it => Point(it._1.get(), it._2.copyBytes()))
+  def loadSIFTs(sc: SparkContext, fileName: String): RDD[Point] = sc
+      .sequenceFile(fileName, classOf[IntWritable], classOf[BytesWritable]) //data is serialized as (IntWritable, BytesWritable)
+      .map(it => Point(it._1.get(), it._2.copyBytes())) //deep-copying data
 }
